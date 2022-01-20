fc074b43b62e830bd042cb65f702013c4e3fb174
Andrew Banman <abanman@pnri.org>
2022-01-20 14:27:26 -0800

    WIP

--- tools/timings.py
+++ tools/timings.py
@@ -14,19 +14,33 @@ def matrix(n,m,b):
 def run_times(data, search, n):
     search.load_ndarray(data)
     search.start() # to prime caches...
-    return timeit.timeit("search.start()", setup="from __main__ import search", number=n)
-
+    return timeit.timeit("search.start()", setup="from __main__ import search", number=n) / float(n)
 
 search = lm.Search()
 search.cache_enabled = False
-search.probability_algorithm = "vector"
+search.probability_algorithm = "bitset"
+#search.probability_algorithm = "vector"
 search.measure = "symmetricdelta"
+search.cache_enabled = False
+search.ranks = 1
 
-for m in range(100, 1100, 100):
-    n = 100
-    b = 2
-    n = 100
-    q = 1000
+print("%s,%s,%s,%s,%s,%s,%s" %('n','d','m','b','q','vector','bitset'))
+#ms = [100, 1000, 5000, 10000, 15000, 20000, 250000]
+ms = list(range(1000,10000,1000))
+for m in ms:
+    b = 6
+    d = 3
+    n = 20
+    q = 50
+    ts = lm.TupleSpace()
+    vs = list(range(0,n))
+    ts.addVariableGroup("all", list(range(0,n)))
+    ts.addVariableGroupTuple([0]*d)
+    search.tuple_space = ts
+    T = ts.count_tuples()
     data = matrix(n=n, m=m, b=b)
-    t = run_times(data, search, q) / float(q)
-    print("%d,%d,%d,%f,q" %(n,m,b,t,q))
+    search.probability_algorithm = "bitset"
+    tb = run_times(data, search, q) / float(T)
+    search.probability_algorithm = "vector"
+    tv = run_times(data, search, q) / float(T)
+    print("%d,%d,%d,%d,%d,%f,%f" %(n,d,m,b,q,tv,tb))
