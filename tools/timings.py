1d3710f426da1acf11a712562f44dfa404eb5d09
Andrew Banman <abanman@pnri.org>
2022-03-10 09:55:39 -0800

    Fix timer, make module

--- tools/timings.py
+++ tools/timings.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pandas as pd
 import libmist as lm
-import timeit
+import time
 import datetime
 
 global search
@@ -13,54 +13,56 @@ global search
 def matrix(n,m,b):
     return np.random.randint(0,b,size=(n,m)).astype('int8')
 
-def run_times(data, search, n):
-    search.load_ndarray(data)
+def run_times(search, n):
     search.start() # to prime caches...
-    return timeit.timeit("search.start()", setup="from __main__ import search", number=n) / float(n)
+    times = []
+    for i in range(0,n):
+        t1 = time.time()
+        search.start()
+        t2 = time.time()
+        times = times + [t2 - t1]
+    return np.average(times) / float(n)
 
-search = lm.Search()
-search.cache_enabled = False
-search.probability_algorithm = "bitset"
-#search.probability_algorithm = "vector"
-search.measure = "symmetricdelta"
-search.cache_enabled = False
-
-# parameters
-q = 50                                  # number of samples per timing
-rs = [1]                                # number of threads
-ms = [100,200,300]                      # samples
-ns = [10,20,30]                         # variables
-bs = [3,4]
-ds = [2]
-
-# header
-print("%s,%s,%s,%s,%s,%s,%s" %('n','d','m','b','q','vector','bitset'))
-df = pd.DataFrame(columns=['n','d','m','b','q','vector','bitset'])
-
-jobs = len(ns)*len(bs)*len(ms)*len(ds)*len(rs)
-i = 1
-
-for n in ns:
-    for m in ms:
-        for b in bs:
-            data = matrix(n=n, m=m, b=b)
-            for d in ds:
-                ts = lm.TupleSpace()
-                vs = list(range(0,n))
-                ts.addVariableGroup("all", list(range(0,n)))
-                ts.addVariableGroupTuple([0]*d)
-                search.tuple_space = ts
-                for r in rs:
-                    search.ranks = r
-                    search.probability_algorithm = "bitset"
-                    tb = run_times(data, search, q)
-                    search.probability_algorithm = "vector"
-                    tv = run_times(data, search, q)
-                    print("[%02d/%02d : %s] %d,%d,%d,%d,%d,%f,%f" %(i,jobs,datetime.datetime.now(),n,d,m,b,q,tv,tb))
-                    # add to results
-                    row = pd.DataFrame({'n':[n],'m':[m],'d':[d],'b':[b],'q':[q],'vector':[tv],'bitset':[tb]})
-                    df = df.append(row, ignore_index = True)
-                    i = i + 1
-
-df.to_csv("timings.csv", index=False)
+def time_mist(q = 50,
+              progress = False,
+              rs = [1],
+              ms = [10,30,60,90,120],
+              ns = [10,20,30],
+              bs = [2,3,4],
+              ds = [2],
+              cs = [False,True],
+              algs = ['vector','bitset']):
+    search = lm.Search()
+    search.measure = "symmetricdelta"
+    df = pd.DataFrame(columns=['n','d','m','b','q','cache','alg','time'])
+    if (progress):
+        print("%s" %(','.join(df.columns)))
+    jobs = len(ns)*len(bs)*len(ms)*len(ds)*len(rs)*len(cs)*len(algs)
+    i = 1
+    for n in ns:
+        for m in ms:
+            for b in bs:
+                data = matrix(n=n, m=m, b=b)
+                search.load_ndarray(data)
+                for d in ds:
+                    ts = lm.TupleSpace()
+                    vs = list(range(0,n))
+                    ts.addVariableGroup("all", list(range(0,n)))
+                    ts.addVariableGroupTuple([0]*d)
+                    search.tuple_space = ts
+                    for r in rs:
+                        search.ranks = r
+                        for alg in algs:
+                            search.probability_algorithm = alg
+                            for c in cs:
+                                search.cache_enabled = c
+                                t = run_times(search, q)
+                                row = pd.DataFrame(
+                                        {'n':[n],'m':[m],'d':[d],'b':[b],'q':[q],'alg':[alg],'cache':[c],'time':[t]}
+                                        )
+                                if (progress):
+                                    print("[%02d/%02d : %s]\n%s" %(i,jobs,datetime.datetime.now(),str(row)))
+                                df = df.append(row, ignore_index = True)
+                                i = i + 1
+    return df
 
